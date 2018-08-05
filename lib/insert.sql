9368bf391018352d365a70cacefc7aea3df351ff
Vicki Hamm <vicki.cello@gmail.com>
2018-08-05 21:01:24 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -22,7 +22,6 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ('Dum
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ('Marco', 'I am a duck', 'animorph', 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ('Tobias', 'I am a squid', 'animorph', 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ('Rachel', 'I am a gorilla', 'animorph', 2, 2);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ('Marco', 'I am a duck', 'animorph', 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ('Cassie', 'I am a wolf', 'animorph', 2, 2);
 
 INSERT INTO character_books (book_id, character_id) VALUES (1, 1), (1, 2), (1, 3), (2, 4), (3, 2), (2, 2), (2, 3), (2, 1), (4, 5), (5, 5), (6, 5), (4, 6), (5, 6), (6, 6), (5, 7), (4, 8);
