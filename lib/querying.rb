5aa3ed0525372e56564c560138715135bdc08660
Vicki Hamm <vicki.cello@gmail.com>
2018-08-05 21:03:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,10 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) 
+  DESC LIMIT 1;"
 end
 
 
